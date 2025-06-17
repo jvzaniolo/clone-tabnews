eba26e680c51177b2205995c237472f2def1cabf
João Vitor <jv.zaniolo@gmail.com>
2025-06-17 20:26:37 -0300

    add database status to `/status`

--- infra/database.js
+++ infra/database.js
@@ -1,19 +1,24 @@
-import { Client } from 'pg'
+import { Client } from "pg";
 
-async function query(queryObject) {
+async function query(config, values) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  })
-  await client.connect()
-  const result = await client.query(queryObject)
-  await client.end()
-  return result
+  });
+  await client.connect();
+  try {
+    const result = await client.query(config, values);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
-  query: query,
-}
+  query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
-import database from 'infra/database'
+import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log(result.rows)
-  response.status(200).json({ chave: 'Alunos do curso.dev são pessoas acima da média!' })
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionQuery = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionQuery.rows[0].server_version;
+
+  const maxConnectionsQuery = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsQuery.rows[0].max_connections;
+
+  const openedConnectionsQuery = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    [process.env.POSTGRES_DB],
+  );
+  const openedConnections = openedConnectionsQuery.rows[0].count;
+
+  return response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        opened_connections: openedConnections,
+        max_connections: parseInt(maxConnections),
+      },
+    },
+  });
 }
 
-export default status
+export default status;
