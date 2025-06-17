5ab4b55979a87c68820c0f9211bf0589bddf23b1
João Vitor <jv.zaniolo@gmail.com>
2025-06-17 14:02:14 -0300

    add `database` environment variables

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,9 @@
+import database from '../../../../infra/database'
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;')
+  console.log(result.rows)
+  response.status(200).json({ chave: 'Alunos do curso.dev são pessoas acima da média!' })
+}
+
+export default status
