cbe67a596703836d20106bc4c228bb4b4388fc41
João Vitor <jv.zaniolo@gmail.com>
2025-06-23 19:05:08 -0300

    add `/api/v1/migrations` endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,39 @@
+import { join } from 'node:path';
+import migrationRunner from 'node-pg-migrate';
+import database from 'infra/database';
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  /** @type {import('node-pg-migrate').RunnerOption} */
+  const defaultMigrationOptions = {
+    dbClient,
+    dir: join('infra', 'migrations'),
+    dryRun: false,
+    direction: 'up',
+    verbose: true,
+    migrationsTable: 'pgmigrations',
+  };
+
+  if (request.method === 'GET') {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === 'POST') {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
+}
