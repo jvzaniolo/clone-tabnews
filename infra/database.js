a3bc2b920423c314081470fa647281221ad7bc06
João Vitor <jv.zaniolo@gmail.com>
2025-06-23 16:18:57 -0300

    prettier

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,4 @@
-import { Client } from "pg";
+import { Client } from 'pg';
 
 async function query(config, values) {
   const client = new Client({
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,17 +1,17 @@
-import database from "infra/database";
+import database from 'infra/database';
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionQuery = await database.query("SHOW server_version;");
+  const databaseVersionQuery = await database.query('SHOW server_version;');
   const databaseVersion = databaseVersionQuery.rows[0].server_version;
 
-  const maxConnectionsQuery = await database.query("SHOW max_connections;");
+  const maxConnectionsQuery = await database.query('SHOW max_connections;');
   const maxConnections = maxConnectionsQuery.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
   const openedConnectionsQuery = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1',
     [databaseName],
   );
   const openedConnections = openedConnectionsQuery.rows[0].count;
