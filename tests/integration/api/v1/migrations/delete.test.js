fd358246cdfc4ce46109f730c1446332c9c63dd6
João Vitor <jv.zaniolo@gmail.com>
2025-06-26 13:56:49 -0300

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- tests/integration/api/v1/migrations/delete.test.js
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -1,11 +1,9 @@
 import database from 'infra/database';
-
-async function cleanDatabase() {
-  await database.query('drop schema public cascade; create schema public;');
-}
+import orchestrator from 'tests/orchestrator';
 
 beforeAll(async () => {
-  await cleanDatabase();
+  await orchestrator.waitForAllServices();
+  await database.query('drop schema public cascade; create schema public;');
 });
 
 test('DELETE to /api/v1/migrations should return 405', async () => {
@@ -17,7 +15,6 @@ test('DELETE to /api/v1/migrations should return 405', async () => {
   const response2 = await fetch('http://localhost:3000/api/v1/status');
   expect(response2.status).toBe(200);
   const response2Body = await response2.json();
-  console.log(response2Body);
 
   expect(response2Body.dependencies.database.opened_connections).toBe(1);
 });
